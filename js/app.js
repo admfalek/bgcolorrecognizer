17d9220f938e9c71c958dec0ab0332af4888fcd2
Adam Fałek <admfalek@gmail.com>
2018-06-11 13:56:45 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const network = new brain.NeuralNetwork();
 
 network.train([
 	// training dataset
-	{ input: { r: 0.62, g: 0.72, b: 0.88 }, output: { light: 1 } },
+  { input: { r: 0.62, g: 0.72, b: 0.88 }, output: { light: 1 } },
   { input: { r: 0.1, g: 0.84, b: 0.72 }, output: { light: 1 } },
   { input: { r: 0.33, g: 0.24, b: 0.29 }, output: { dark: 1 } },
   { input: { r: 0.74, g: 0.78, b: 0.86 }, output: { light: 1 } },
